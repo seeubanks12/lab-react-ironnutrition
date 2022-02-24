5d6095cfb86a1b3b443d61e129760f1e281c676e
Eliza <seeubanks12@gmail.com>
2022-02-24 09:25:18 -0500

    iteration 2-3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import './App.css';
 import foodsArr from './foods.json';
+import FoodBox from './components/FoodBox';
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
 
 function App() {
   const [foods, setFoods] = React.useState(foodsArr);
   return (
     <div className="App">
       {/* food is prop */}
+      {/* <FoodBox
+        food={{
+          name: 'Orange',
+          calories: 85,
+          image: 'https://i.imgur.com/abKGOcv.jpg',
+          servings: 1,
+        }}
+      />
       {foods.map((food) => {
         return (
           <div>
             <p>{food.name}</p>
-            <img src={food.image} width={100} alt="food"/>
+            <img src={food.image} width={100} alt="food" />
           </div>
         );
-      })}
+      })} */}
+      <Divider> Food List</Divider>
+      <Row>
+        {foods.map((food) => {
+          return <FoodBox food={food}/>
+        })}
+      </Row>
     </div>
   );
 }
